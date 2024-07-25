4426c86161de64fd1c4fc9630da0bcc0693124b7
Sanvi Ojha <118836410+SANVIOJHA@users.noreply.github.com>
2024-07-25 12:10:37 +0530

    Delete blogging-website-backend directory

--- blogging-website-backend/config/allowedOrigins.js
+++
@@ -1,9 +0,0 @@
-
-const allowedOrigins = [
-    'http://localhost:5173',
-    'http://localhost:4200',
-    'https://loquacious-seahorse-614b70.netlify.app/'
-
-];
-
-module.exports = allowedOrigins;
